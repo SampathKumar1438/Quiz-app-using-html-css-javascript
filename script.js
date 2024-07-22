acb2e88973b176487ea53ece4508690c5cf8761c
SAMPATH KUMAR <141661993+SampathKumar1438@users.noreply.github.com>
2024-07-22 17:18:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function select() {
+  var result = document.getElementById("crt");
+  result.addEventListener("click", function () {
+    this.style.background = "green";
+  });
+}
+function select_1() {
+    var result = document.getElementById("select1");
+    result.addEventListener("click", function () {
+      this.style.background = "red";
+      alert("try again");
+    });
+  }
+  function select_2() {
+    var result = document.getElementById("select2");
+    result.addEventListener("click", function () {
+      this.style.background = "red";
+      alert("try again");
+    });
+  }
+  function select_3() {
+    var result = document.getElementById("select3");
+    result.addEventListener("click", function () {
+      this.style.background = "red";
+      alert("try again");
+    });
+  }
+  
